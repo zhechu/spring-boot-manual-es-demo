9a56a85e879c91159d9d2aa4f8671ac53ca7f06c
zhechu <ling-yu-wang@qq.com>
2019-10-07 17:29:16 +0800

    初始化

---
+++ src/main/java/com/wise/elasticsearch/demo/document/ProfileDocument.java
@@ -0,0 +1,22 @@
+package com.wise.elasticsearch.demo.document;
+
+public class ProfileDocument {
+    private String id;
+    private String name;
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/wise/elasticsearch/demo/util/Constant.java
@@ -0,0 +1,6 @@
+package com.wise.elasticsearch.demo.util;
+
+public interface Constant {
+    String INDEX = "lead";
+    String TYPE = "_doc";
+}
